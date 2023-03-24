6f45ef6776aae72641aa1d9fea7bb38d13d5c0af
AGUASISABELLA <123745048+AGUASISABELLA@users.noreply.github.com>
2023-03-24 09:13:27 -0500

    Add files via upload

---
+++ lab4/laboratorio4/src/Paciente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+class Paciente {
+    String nombre ;
+    int edad;
+    String afiliado;
+   String condicionEspecial;
+    
+    Paciente(String nombre,int edad,String afiliaciones,String condicionEspecial){
+    }
+
+    String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
