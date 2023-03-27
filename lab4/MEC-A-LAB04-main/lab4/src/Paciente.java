faf033c8e98f3cc99e7a564859f225e483cf7be7
AGUASISABELLA <123745048+AGUASISABELLA@users.noreply.github.com>
2023-03-26 23:30:21 -0500

    Add files via upload

---
+++ lab4/MEC-A-LAB04-main/lab4/src/Paciente.java
@@ -0,0 +1,13 @@
+class Paciente {
+    String nombre ;
+    int edad;
+    String afiliado;
+   String condicionEspecial;
+    
+    Paciente(String nombre,int edad,String afiliaciones,String condicionEspecial){
+    }
+
+    String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
